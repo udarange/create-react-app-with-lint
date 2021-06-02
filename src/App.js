2dc77cd7a6055ee7463af85e6fd2c43708dcc91f
Archana <udarange@gmail.com>
2021-06-02 10:51:14 +0530

    remove unnecessary packages

--- src/App.js
+++ src/App.js
@@ -5,20 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"
-        />
-        <p>
-
-
-
-
-
-
-
-
-          Hello
-        </p>
-        <a className="App-link"
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello</p>
+        <a
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
